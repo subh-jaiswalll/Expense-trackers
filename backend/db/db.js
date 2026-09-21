a40a786c2870d793542b76ab2f3ddb1cfd0c3d4c
atulcodes-tech <subhkaranjaiswal41@gmail.com>
2026-09-21 21:01:13 +0530

    Backend Deployment

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
-
-const Sequelize = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -8,9 +6,16 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        logging: false
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+        dialectOptions: {
+            ssl: {
+                require: true
+            }
+        }
     }
 );
+
+module.exports = sequelize;
 
 module.exports = sequelize;
